5fbd0ca63c1fa369a16ec9eddef3c036def09b78
mauconig <mauricioconigliaro1@gmail.com>
2023-12-02 11:22:00 -0300

    solved!

--- Day1/solution.py
+++ Day1/solution.py
@@ -1,3 +1,26 @@
-i = [1,2,3,4]
-temp = 0
-print(i[0]*10 + i[-1]);
+ints = '123456789'
+
+# Replace 'your_file.txt' with the path to your text file
+file_path = 'input.txt'
+
+# Open and read the file
+with open(file_path, 'r') as file:
+    lines = file.readlines()
+
+def dayOne(text, ints, arr = []):
+    tempArr = []
+    for i in range(len(text)):
+        if text[i] in ints:
+            tempArr.append(int(text[i]))
+    arr.append(tempArr[0]*10 + tempArr[-1])
+    return arr
+
+solution = []
+for i in range(len(lines)):
+    dayOne(lines[i], ints, solution)
+
+finalSolution = 0
+for i in range(len(solution)):
+    finalSolution = finalSolution + solution[i]
+
+print(finalSolution)
